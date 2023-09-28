a4d195b06b1ccd28272736de527735211b67ca6f
jackson amatti <amatti.tec@gmail.com>
2023-09-28 09:59:58 -0300

    feat: create feed user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
-class UsersController < ApplicationController
-  def destroy
-  @user = User.find(params[:id])
-  @user.destroy
-  redirect_to root
-  end
+class UsersController < ApplicationCon
+  # def destroy
+  # @user = User.find(params[:id])
+  # @user.destroy
+  # redirect_to root
+  # end
 end
